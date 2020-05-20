f021ee62c248d5936f9641de19a20af7b9d35c8a
Designer225 <designer2553@gmail.com>
2020-05-20 04:00:27 -0700

    tentative localisation support

---
+++ BattleRegen/BattleRegenSettings.Localisation.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleRegen
+{
+    public partial class BattleRegenSettings
+    {
+        internal const string ModNameText   = "Battle Regeneration",
+
+            RegenValuesName                 = "{=BattleRegen_RegenValuesName}Regeneration Values",
+            RegenAmountName                 = "{=BattleRegen_RegenAmountName}Regen Amount (Percents) Per Second",
+            RegenAmountHint                 = "{=BattleRegen_RegenAmountHint}What percent of total health to be regenerated every second. (Regen is applied continuously with this mod.) Default is 1%.",
+            MedicineBoostName               = "{=BattleRegen_MedicineBoostName}Medicine Boost (Percents)",
+            MedicineBoostHint               = "{=BattleRegen_MedicineBoostHint}Regen is increased by this percentage for every 50 points in a human agent's medicine skill. Riders affect regen for their mounts at standard efficiency. Bonuses stack additively. Default is 50%.",
+            CommanderMedicineBoostName      = "{=BattleRegen_CommanderMedicineBoostName}Medicine Boost (Percents) For Commanders",
+            CommanderMedicineBoostHint      = "{=BattleRegen_CommanderMedicineBoostHint}Regen is increased by this percentage for every 50 points in a commander's medicine skills. Bonuses stack additively. Default is 25%.",
+            XpGainName                      = "{=BattleRegen_XpGainName}XP Gain Per Full Health",
+            XpGainHint                      = "{=BattleRegen_XpGainHint}How much XP is gained when an agent or its rider (if applicable) heals enough to refill its health bar from 0 to max health. XP gain is continuous. Default is 1.",
+            CommanderXpGainName             = "{=BattleRegen_CommanderXpGainName}XP Gain Per Full Health For Commanders",
+            CommanderXpGainHint             = "{=BattleRegen_CommanderXpGainHint}How much XP is gained when a commander heals an agent enough to refill its health bar from 0 to max health. XP gain is continuous. Default is 0.1.",
+            
+            RegenSettingsName               = "{=BattleRegen_RegenSettingsName}Regeneration Settings",
+            RegenModelDropdownName          = "{=BattleRegen_RegenModelDropdownName}Regeneration Model",
+            RegenModelDropdownHint          = "{=BattleRegen_RegenModelDropdownHint}Determines the model used for regenerating health. See Nexus Mods page (https://www.nexusmods.com/mountandblade2bannerlord/mods/1432) for more details. Default is Linear.",
+            ApplyToPlayerName               = "{=BattleRegen_ApplyToPlayerName}Apply To Player",
+            ApplyToPlayerHint               = "{=BattleRegen_ApplyToPlayerHint}Whether the player should receive passive regen. Default is true.",
+            ApplyToCompanionsName           = "{=BattleRegen_ApplyToCompanionsName}Apply To Companions",
+            ApplyToCompanionsHint           = "{=BattleRegen_ApplyToCompanionsHint}Whether the player's companions should receive passive regen. Default is true.",
+            ApplyToAlliedHeroesName         = "{=BattleRegen_ApplyToAlliedHeroesName}Apply To Allied Heroes",
+            ApplyToAlliedHeroesHint         = "{=BattleRegen_ApplyToAlliedHeroesHint}Whether the player's allied heroes should receive passive regen. Default is true.",
+            ApplyToPartyTroopsName          = "{=BattleRegen_ApplyToPartyTroopsName}Apply To Party Troops",
+            ApplyToPartyTroopsHint          = "{=BattleRegen_ApplyToPartyTroopsHint}Whether the player's troops should receive passive regen. Default is true.",
+            ApplyToAlliedTroopsName         = "{=BattleRegen_ApplyToAlliedTroopsName}Apply To Allied Troops",
+            ApplyToAlliedTroopsHint         = "{=BattleRegen_ApplyToAlliedTroopsHint}Whether the player's allied troops should receive passive regen. Default is true.",
+            ApplyToEnemyHeroesName          = "{=BattleRegen_ApplyToEnemyHeroesName}Apply To Enemy Heroes",
+            ApplyToEnemyHeroesHint          = "{=BattleRegen_ApplyToEnemyHeroesHint}Whether the player's enemy heroes should receive passive regen. Default is true.",
+            ApplyToEnemyTroopsName          = "{=BattleRegen_ApplyToEnemyTroopsName}Apply To Enemy Troops",
+            ApplyToEnemyTroopsHint          = "{=BattleRegen_ApplyToEnemyTroopsHint}Whether the player's enemy troops should receive passive regen. Default is true.",
+            ApplyToMountName                = "{=BattleRegen_ApplyToMountName}Apply To Mounts",
+            ApplyToMountHint                = "{=BattleRegen_ApplyToMountHint}Whether mounts should receive passive regen. Mounts regenerate regardless of their rider's allegiance. Default is true.",
+            DebugName                       = "{=BattleRegen_DebugName}Debug Mode",
+            DebugHint                       = "{=BattleRegen_DebugHint}Whether to print debug outputs to log (in C:/ProgramData/Mount and Blade II Bannerlord/logs folder). Default is false.",
+
+            UseSliderForRegenModelName      = "{=BattleRegen_UseSliderForRegenModelName}Use Slider for Regen Model",
+            UseSliderForRegenModelHint      = "{=BattleRegen_UseSliderForRegenModelHint}Should the mod use a slider for regen instead of dropdown options.",
+            SliderRegenModelName            = "{=BattleRegen_SliderRegenModelName}Regeneration Model",
+            SliderRegenModelHint            = "{=BattleRegen_SliderRegenModelHint}Determines the model used for regenerating health. 'Linear' is 1, 'Quadratic' is 2, and 'EVE Online' is 3. See Nexus Mods page (https://www.nexusmods.com/mountandblade2bannerlord/mods/1432) for more details. Default is Linear.";
+    }
+}
--- BattleRegen/BattleRegenSettings.cs
+++ BattleRegen/BattleRegenSettings.cs
@@ -2,55 +2,42 @@
 using MBOptionScreen.Attributes.v2;
 using MBOptionScreen.Data;
 using MBOptionScreen.Settings;
-using TaleWorlds.Localization;
 
 namespace BattleRegen
 {
-    public class BattleRegenSettings : AttributeSettings<BattleRegenSettings>
+    public partial class BattleRegenSettings : AttributeSettings<BattleRegenSettings>
     {
         public override string Id { get; set; } = "D225.BattleRegen";
 
-        public override string ModName => "Battle Regeneration";
+        public override string ModName => ModNameText;
 
         public override string ModuleFolderName => "D225.BattleRegen";
 
         #region Regeneration Values
-        [SettingPropertyFloatingInteger("Regen Amount (Percents) Per Second", 0f, 25f,
-            HintText = "What percent of total health to be regenerated every second. (Regen is applied continuously with this mod.) Default is 1%.",
-            Order = 0, RequireRestart = false)]
-        [SettingPropertyGroup("Regeneration Values")]
+        [SettingPropertyFloatingInteger(RegenAmountName, 0f, 25f, HintText = RegenAmountHint, Order = 0, RequireRestart = false)]
+        [SettingPropertyGroup(RegenValuesName, 0)]
         public float RegenAmount { get; set; } = 1f;
 
-        [SettingPropertyFloatingInteger("Medicine Boost (Percents)", 0f, 200f,
-            HintText = "Regen is increased by this percentage for every 50 points in a human agent's medicine skill. Riders affect regen for their mounts at standard efficiency. Bonuses stack additively. Default is 50%.",
-            Order = 1, RequireRestart = false)]
-        [SettingPropertyGroup("Regeneration Values", 0)]
+        [SettingPropertyFloatingInteger(MedicineBoostName, 0f, 200f, HintText = MedicineBoostHint, Order = 1, RequireRestart = false)]
+        [SettingPropertyGroup(RegenValuesName)]
         public float MedicineBoost { get; set; } = 50f;
 
-        [SettingPropertyFloatingInteger("Medicine Boost (Percents) For Commanders", 0f, 200f,
-            HintText = "Regen is increased by this percentage for every 50 points in a commander's medicine skills. Bonuses stack additively. Default is 25%.",
-            Order = 2, RequireRestart = false)]
-        [SettingPropertyGroup("Regeneration Values")]
+        [SettingPropertyFloatingInteger(CommanderMedicineBoostName, 0f, 200f, HintText = CommanderMedicineBoostHint, Order = 2, RequireRestart = false)]
+        [SettingPropertyGroup(RegenValuesName)]
         public float CommanderMedicineBoost { get; set; } = 25f;
 
-        [SettingPropertyFloatingInteger("XP Gain Per Full Health", 0f, 10f,
-            HintText = "How much XP is gained when an agent or its rider (if applicable) heals enough to refill its health bar from 0 to max health. XP gain is continuous. Default is 1.",
-            Order = 3, RequireRestart = false)]
-        [SettingPropertyGroup("Regeneration Values")]
+        [SettingPropertyFloatingInteger(XpGainName, 0f, 10f, HintText = XpGainHint, Order = 3, RequireRestart = false)]
+        [SettingPropertyGroup(RegenValuesName)]
         public float XpGain { get; set; } = 1f;
 
-        [SettingPropertyFloatingInteger("XP Gain Per Full Health For Commanders", 0f, 10f,
-            HintText = "How much XP is gained when a commander heals an agent enough to refill its health bar from 0 to max health. XP gain is continuous. Default is 0.1.",
-            Order = 4, RequireRestart = false)]
-        [SettingPropertyGroup("Regeneration Values")]
+        [SettingPropertyFloatingInteger(CommanderXpGainName, 0f, 10f, HintText = CommanderXpGainHint, Order = 4, RequireRestart = false)]
+        [SettingPropertyGroup(RegenValuesName)]
         public float CommanderXpGain { get; set; } = 0.1f;
         #endregion
 
         #region Regeneration Settings
-        [SettingPropertyDropdown("Regeneration Model",
-            HintText = "Determines the model used for regenerating health. See Nexus Mods page (https://www.nexusmods.com/mountandblade2bannerlord/mods/1432) for more details. Default is Linear.",
-            Order = 5, RequireRestart = false)]
-        [SettingPropertyGroup("Regeneration Settings", 1)]
+        [SettingPropertyDropdown(RegenModelDropdownName, HintText = RegenModelDropdownHint, Order = 5, RequireRestart = false)]
+        [SettingPropertyGroup(RegenSettingsName, 1)]
         public DefaultDropdown<BattleRegenModel> RegenModelDropdown { get; set; } = new DefaultDropdown<BattleRegenModel>(new BattleRegenModel[]
         {
             BattleRegenModel.Linear,
@@ -58,72 +45,50 @@ namespace BattleRegen
             BattleRegenModel.EveOnline
         }, 0);
 
-        [SettingPropertyBool("Apply To Player",
-            HintText = "Whether the player should receive passive regen. Default is true.",
-            Order = 6, RequireRestart = false)]
-        [SettingPropertyGroup("Regeneration Settings")]
+        [SettingPropertyBool(ApplyToPlayerName, HintText = ApplyToPlayerHint, Order = 6, RequireRestart = false)]
+        [SettingPropertyGroup(RegenSettingsName)]
         public bool ApplyToPlayer { get; set; } = true;
 
-        [SettingPropertyBool("Apply To Companions",
-            HintText = "Whether the player's companions should receive passive regen. Default is true.",
-            Order = 7, RequireRestart = false)]
-        [SettingPropertyGroup("Regeneration Settings")]
+        [SettingPropertyBool(ApplyToCompanionsName, HintText = ApplyToCompanionsHint, Order = 7, RequireRestart = false)]
+        [SettingPropertyGroup(RegenSettingsName)]
         public bool ApplyToCompanions { get; set; } = true;
 
-        [SettingPropertyBool("Apply To Allied Heroes",
-            HintText = "Whether the player's allied heroes should receive passive regen. Default is true.",
-            Order = 8, RequireRestart = false)]
-        [SettingPropertyGroup("Regeneration Settings")]
+        [SettingPropertyBool(ApplyToAlliedHeroesName, HintText = ApplyToAlliedHeroesHint, Order = 8, RequireRestart = false)]
+        [SettingPropertyGroup(RegenSettingsName)]
         public bool ApplyToAlliedHeroes { get; set; } = true;
 
-        [SettingPropertyBool("Apply To Party Troops",
-            HintText = "Whether the player's troops should receive passive regen. Default is true.",
-            Order = 9, RequireRestart = false)]
-        [SettingPropertyGroup("Regeneration Settings")]
+        [SettingPropertyBool(ApplyToPartyTroopsName, HintText = ApplyToPartyTroopsHint, Order = 9, RequireRestart = false)]
+        [SettingPropertyGroup(RegenSettingsName)]
         public bool ApplyToPartyTroops { get; set; } = true;
 
-        [SettingPropertyBool("Apply To Allied Troops",
-            HintText = "Whether the player's allied troops should receive passive regen. Default is true.",
-            Order = 10, RequireRestart = false)]
-        [SettingPropertyGroup("Regeneration Settings")]
+        [SettingPropertyBool(ApplyToAlliedTroopsName, HintText = ApplyToAlliedTroopsHint, Order = 10, RequireRestart = false)]
+        [SettingPropertyGroup(RegenSettingsName)]
         public bool ApplyToAlliedTroops { get; set; } = true;
 
-        [SettingPropertyBool("Apply To Enemy Heroes",
-            HintText = "Whether the player's enemy heroes should receive passive regen. Default is true.",
-            Order = 11, RequireRestart = false)]
-        [SettingPropertyGroup("Regeneration Settings")]
+        [SettingPropertyBool(ApplyToEnemyHeroesName, HintText = ApplyToEnemyHeroesHint, Order = 11, RequireRestart = false)]
+        [SettingPropertyGroup(RegenSettingsName)]
         public bool ApplyToEnemyHeroes { get; set; } = true;
 
-        [SettingPropertyBool("Apply To Enemy Troops",
-            HintText = "Whether the player's enemy troops should receive passive regen. Default is true.",
-            Order = 12, RequireRestart = false)]
-        [SettingPropertyGroup("Regeneration Settings")]
+        [SettingPropertyBool(ApplyToEnemyTroopsName, HintText = ApplyToEnemyTroopsHint, Order = 12, RequireRestart = false)]
+        [SettingPropertyGroup(RegenSettingsName)]
         public bool ApplyToEnemyTroops { get; set; } = true;
 
-        [SettingPropertyBool("Apply To Mounts",
-            HintText = "Whether mounts should receive passive regen. Mounts regenerate regardless of their rider's allegiance. Default is true.",
-            Order = 13, RequireRestart = false)]
-        [SettingPropertyGroup("Regeneration Settings")]
+        [SettingPropertyBool(ApplyToMountName, HintText = ApplyToMountHint, Order = 13, RequireRestart = false)]
+        [SettingPropertyGroup(RegenSettingsName)]
         public bool ApplyToMount { get; set; } = true;
 
-        [SettingPropertyBool("Debug Mode",
-            HintText = "Whether to print debug outputs to log (in C:/ProgramData/Mount and Blade II Bannerlord/logs folder). Default is false.",
-            Order = 14, RequireRestart = false)]
-        [SettingPropertyGroup("Regeneration Settings")]
+        [SettingPropertyBool(DebugName, HintText = DebugHint, Order = 14, RequireRestart = false)]
+        [SettingPropertyGroup(RegenSettingsName)]
         public bool Debug { get; set; } = false;
         #endregion
 
         #region Emergency Settings
-        [SettingPropertyBool("Use Slider for Regen Model",
-            HintText = "Should the mod use a slider for regen instead of dropdown options.",
-            Order = 4, RequireRestart = false)]
-        [SettingPropertyGroup("Use Slider for Regen Model", 2, true)]
+        [SettingPropertyBool(UseSliderForRegenModelName, HintText = UseSliderForRegenModelHint, Order = 4, RequireRestart = false)]
+        [SettingPropertyGroup(UseSliderForRegenModelName, 2, true)]
         public bool UseSliderForRegenModel { get; set; } = false;
 
-        [SettingPropertyInteger("Regeneration Model", (int)BattleRegenModel.Linear, (int)BattleRegenModel.EveOnline,
-            HintText = "Determines the model used for regenerating health. 'Linear' is 1, 'Quadratic' is 2, and 'EVE Online' is 3. See Nexus Mods page (https://www.nexusmods.com/mountandblade2bannerlord/mods/1432) for more details. Default is Linear.",
-            Order = 5, RequireRestart = false)]
-        [SettingPropertyGroup("Use Slider for Regen Model")]
+        [SettingPropertyInteger(SliderRegenModelName, (int)BattleRegenModel.Linear, (int)BattleRegenModel.EveOnline, HintText = SliderRegenModelHint, Order = 5, RequireRestart = false)]
+        [SettingPropertyGroup(UseSliderForRegenModelName)]
         public int SliderRegenModel { get; set; } = (int)BattleRegenModel.Linear;
         #endregion
 
